6f509a446aeb621b1b7da35e4c04b1b6e98c9eab
dudwls901 <dudwls901@naver.com>
2025-08-31 19:14:28 +0900

    Library Convention Plugin 내부 pluginManager apply() 접근 방식 변경

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,10 +1,12 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.apply
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) = with(project) {
-        apply(plugin = "com.android.library")
+        with(pluginManager) {
+            apply("com.android.library")
+        }
+
         /*todo*/
     }
 }

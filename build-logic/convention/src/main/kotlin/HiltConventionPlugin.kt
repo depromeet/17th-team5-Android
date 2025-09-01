d934ab5c8d31c5bb4393792b882159c68db836ad
MinseoSONG <mins041600@naver.com>
2025-09-01 16:40:20 +0900

    Hilt Convention Plugin 생성

---
+++ build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
@@ -0,0 +1,28 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class HiltConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+        pluginManager.withPlugin("org.jetbrains.kotlin.android") {
+            pluginManager.apply("com.google.devtools.ksp")
+        }
+        pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+            pluginManager.apply("com.google.devtools.ksp")
+        }
+
+        pluginManager.withPlugin("com.android.base") {
+            pluginManager.apply("com.google.dagger.hilt.android")
+
+            dependencies {
+                add("implementation", libs.findLibrary("hilt-android").get())
+                add("ksp", libs.findLibrary("hilt-android-compiler").get())
+            }
+        }
+    }
+
+}

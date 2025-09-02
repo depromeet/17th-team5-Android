30eebaebc149942d77d9e06d1b79584a111bd3c5
MinseoSONG <mins041600@naver.com>
2025-09-02 19:28:15 +0900

    AndroidHiltConventionPlugin 적용 방식 변경

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -1,28 +1,20 @@
+import com.depromeet.team5.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
+
     override fun apply(target: Project) = with(target) {
-        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
-
-        pluginManager.withPlugin("org.jetbrains.kotlin.android") {
-            pluginManager.apply("com.google.devtools.ksp")
-        }
-        pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-            pluginManager.apply("com.google.devtools.ksp")
+        with(pluginManager) {
+            apply("com.google.dagger.hilt.android")
+            apply("com.google.devtools.ksp")
         }
 
-        pluginManager.withPlugin("com.android.base") {
-            pluginManager.apply("com.google.dagger.hilt.android")
-
-            dependencies {
-                add("implementation", libs.findLibrary("hilt-android").get())
-                add("ksp", libs.findLibrary("hilt-android-compiler").get())
-            }
+        dependencies {
+            add("implementation", libs.findLibrary("hilt-android").get())
+            add("ksp", libs.findLibrary("hilt-android-compiler").get())
         }
     }
-
 }
+

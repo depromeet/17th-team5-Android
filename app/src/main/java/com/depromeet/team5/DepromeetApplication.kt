6f1005e8fa3df846bcf3938a40ebbcc517c46c11
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-09-05 14:32:48 +0900

    Application 오타 수정

--- app/src/main/java/com/depromeet/team5/DepromeetApplication.kt
+++ app/src/main/java/com/depromeet/team5/DepromeetApplication.kt
@@ -4,6 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DeprommetApplication : Application() {
+class DepromeetApplication : Application() {
 
 }

c2006f898172482e257263117259f2847f1d99f7
dudwls901 <dudwls901@naver.com>
2025-09-11 19:15:09 +0900

    TestApiSource 구현 및 datasource 연결

--- core/remotedatasource/src/main/java/com/depromeet/team5/core/remotedatasource/RemoteDataSourceImpl.kt
+++ core/remotedatasource/src/main/java/com/depromeet/team5/core/remotedatasource/RemoteDataSourceImpl.kt
@@ -2,16 +2,15 @@ package com.depromeet.team5.core.remotedatasource
 
 import com.depromeet.team5.core.data.datasource.RemoteDataSource
 import com.depromeet.team5.core.data.model.TestData
-import com.depromeet.team5.core.remotedatasource.model.TestRemoteResponse
-import kotlinx.coroutines.coroutineScope
+import com.depromeet.team5.core.remotedatasource.apisource.TestApiSource
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
-internal class RemoteDataSourceImpl @Inject constructor() : RemoteDataSource {
+internal class RemoteDataSourceImpl @Inject constructor(
+    private val testApiSource: TestApiSource,
+) : RemoteDataSource {
 
-    override suspend fun getData(): TestData = coroutineScope {
-        TestRemoteResponse("Hello RemoteDataSource World").toData()
-    }
+    override suspend fun getData(): TestData = testApiSource.getTest().toData()
 }

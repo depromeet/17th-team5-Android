0e692040c0fb4deb26e3292ec9375223ec25388c
dudwls901 <dudwls901@naver.com>
2025-09-11 16:33:44 +0900

    core:retrofit 모듈 생성

---
+++ core/retrofit/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.depromeet.team5.library)
+    alias(libs.plugins.depromeet.team5.hilt)
+}
+
+android {
+    namespace = "com.depromeet.team5.core.retrofit"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    implementation(projects.depromeet.core.remotedatasource)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}

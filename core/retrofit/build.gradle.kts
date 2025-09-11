fa0bb10506fb1e2a901ed50d2216eb2273433b3d
dudwls901 <dudwls901@naver.com>
2025-09-11 17:45:10 +0900

    add dependencies

--- core/retrofit/build.gradle.kts
+++ core/retrofit/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.depromeet.team5.library)
     alias(libs.plugins.depromeet.team5.hilt)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -28,4 +29,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(libs.com.squareup.retrofit2.retrofit)
+    implementation(libs.com.jakewharton.retrofit.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.org.jetbrains.kotlinx.kotlinx.serialization.json)
+    implementation(libs.com.squareup.okhttp3.okhttp)
+    implementation(libs.com.squareup.okhttp3.logging.interceptor)
 }

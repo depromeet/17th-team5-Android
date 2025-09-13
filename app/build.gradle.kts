c59d67b6ed9cb00e162e11172e6aa1007e23527d
MinseoSONG <mins041600@naver.com>
2025-09-13 20:21:15 +0900

    :features:search 모듈 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation(projects.core.localdatasource)
     implementation(projects.core.remotedatasource)
     implementation(projects.core.retrofit)
+    implementation(projects.features.search)
     implementation(libs.androidx.core.ktx)
 
     testImplementation(libs.junit)

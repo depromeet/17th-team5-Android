b7dfb8a2b8bbac1126ff995994899c998a9d867d
MinseoSONG <mins041600@naver.com>
2025-09-14 03:35:51 +0900

    사용하지 않는 import 구문 제거

--- features/search/src/main/java/com/depromeet/team5/features/search/component/SearchTextField.kt
+++ features/search/src/main/java/com/depromeet/team5/features/search/component/SearchTextField.kt
@@ -17,11 +17,8 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController

3d175c60aaf75c6cfd5712b544153e8136b28d32
seoganghwi <skh0818@hanyang.ac.kr>
2025-09-15 23:29:04 +0900

    수익률 toggle == on 시, focusing되도록 수정

--- features/retrospect/src/main/java/com/depromeet/team5/features/retrospect/screen/RetrospectScreen.kt
+++ features/retrospect/src/main/java/com/depromeet/team5/features/retrospect/screen/RetrospectScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -36,6 +37,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -49,6 +52,7 @@ import com.depromeet.team5.features.retrospect.R
 import com.depromeet.team5.features.retrospect.screen.annotation.CurrencyUnit
 import com.depromeet.team5.features.retrospect.screen.annotation.KOREAN
 import com.depromeet.team5.features.retrospect.screen.annotation.PERCENT
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -66,6 +70,14 @@ private fun RetrospectScreen(
     onBackPressed: () -> Unit
 ) {
     var returnVisibility by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(returnVisibility) {
+        if (returnVisibility) {
+            delay(300)
+            focusRequester.requestFocus()
+        }
+    }
 
     Column(
         modifier = modifier
@@ -129,7 +141,9 @@ private fun RetrospectScreen(
             )
         ) {
             HedgeTextField(
-                modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 12.dp),
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .padding(start = 20.dp, end = 20.dp, top = 12.dp),
                 label = "수익률",
                 placeholder = "%",
                 unit = PERCENT

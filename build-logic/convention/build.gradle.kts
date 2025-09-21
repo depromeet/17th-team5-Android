4f806d84a8f69aea84d177524cbb124b1bb2d0db
MinseoSONG <mins041600@naver.com>
2025-09-21 23:07:22 +0900

    최상단 build.gradle.kts 파일에 kotlinx-serialization 플러그인 추가

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 //    compileOnly(libs.room.gradlePlugin)
-    implementation(libs.kotlin.serialization.gradlePlugin)
 
     implementation(libs.truth)
 }

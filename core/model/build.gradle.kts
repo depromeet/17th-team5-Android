d2d287aec726a0ddd68671bea51645179bc4452c
seoganghwi <skh0818@hanyang.ac.kr>
2025-09-25 21:22:19 +0900

    core-model 생성

---
+++ core/model/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.depromeet.team5.library)
+}
+
+android {
+    namespace = "com.depromeet.team5.core.model"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.compose.runtime)
+}

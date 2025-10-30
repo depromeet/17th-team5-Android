70f8ca5d82d136370705dbb34c0259b4e2014aee
seoganghwi <skh0818@hanyang.ac.kr>
2025-10-30 17:57:12 +0900

    AssistedInject로 수정

--- features/retrospect/src/main/java/com/depromeet/team5/features/retrospect/screen/RetrospectionViewModel.kt
+++ features/retrospect/src/main/java/com/depromeet/team5/features/retrospect/screen/RetrospectionViewModel.kt
@@ -5,22 +5,30 @@ import androidx.lifecycle.viewModelScope
 import com.depromeet.team5.core.domain.monad.HedgeUiState
 import com.depromeet.team5.core.domain.monad.asUiState
 import com.depromeet.team5.core.domain.usecase.GetPrinciplesUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
-import javax.inject.Inject
 
 
-@HiltViewModel
-class RetrospectionViewModel @Inject constructor(
-    private val getPrincipleUseCase: GetPrinciplesUseCase
+@HiltViewModel(assistedFactory = RetrospectionViewModel.Factory::class)
+class RetrospectionViewModel @AssistedInject constructor(
+    private val getPrincipleUseCase: GetPrinciplesUseCase,
+    @Assisted orderType: String
 ) : ViewModel() {
 
-    val principleUiState = getPrincipleUseCase()
+    val principleUiState = getPrincipleUseCase(orderType)
         .asUiState()
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = HedgeUiState.Loading()
+            initialValue = HedgeUiState.Loading(emptyMap())
         )
+
+    @AssistedFactory
+    interface Factory {
+        fun create(orderType: String): RetrospectionViewModel
+    }
 }

c1c5db1091384048aef01ba27a7ad8829a6b7750
dudwls901 <dudwls901@naver.com>
2025-11-02 21:45:26 +0900

    jsoup, coil 디펜던시 추가

--- features/reasons/build.gradle.kts
+++ features/reasons/build.gradle.kts
@@ -29,5 +29,6 @@ dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.domain)
     implementation(projects.core.navigation)
-
+    implementation(libs.org.jsoup.jsoup)
+    implementation(libs.coil)
 }

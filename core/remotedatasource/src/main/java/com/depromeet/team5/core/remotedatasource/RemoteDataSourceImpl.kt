33707fff61f385f3e1f18dfb9fdc8c9b0f509c72
seoganghwi <skh0818@hanyang.ac.kr>
2025-11-03 19:58:35 +0900

    conflict 해결

--- core/remotedatasource/src/main/java/com/depromeet/team5/core/remotedatasource/RemoteDataSourceImpl.kt
+++ core/remotedatasource/src/main/java/com/depromeet/team5/core/remotedatasource/RemoteDataSourceImpl.kt
@@ -28,8 +28,6 @@ internal class RemoteDataSourceImpl @Inject constructor(
     override suspend fun createAnalysis(body: Map<String, Any?>): AnalysisData =
         hedgeApiSource.createAnalysis(body).toData()
 
-    override suspend fun getPrinciples(): MyPrincipleData =
-        hedgeApiSource.getPrinciples().toData()
     override suspend fun getPrinciples(orderType: String): MyPrincipleData =
         hedgeApiSource.getPrinciples(orderType).toData()
 }

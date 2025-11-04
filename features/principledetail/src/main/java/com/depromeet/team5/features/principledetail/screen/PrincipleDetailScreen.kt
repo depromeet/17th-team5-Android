b6a70aed300d3c4d2d0cb10c1a7bee86bae5009c
seoganghwi <skh0818@hanyang.ac.kr>
2025-11-04 13:45:31 +0900

    material3 Text 사용하도록 수정

--- features/principledetail/src/main/java/com/depromeet/team5/features/principledetail/screen/PrincipleDetailScreen.kt
+++ features/principledetail/src/main/java/com/depromeet/team5/features/principledetail/screen/PrincipleDetailScreen.kt
@@ -16,10 +16,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf

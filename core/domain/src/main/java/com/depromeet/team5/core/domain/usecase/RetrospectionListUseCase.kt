04b3637cb20475d5e0db5fbe25bc4891247b846c
MinseoSONG <mins041600@naver.com>
2025-11-07 23:45:37 +0900

    리턴 타입 명시

--- core/domain/src/main/java/com/depromeet/team5/core/domain/usecase/RetrospectionListUseCase.kt
+++ core/domain/src/main/java/com/depromeet/team5/core/domain/usecase/RetrospectionListUseCase.kt
@@ -1,11 +1,13 @@
 package com.depromeet.team5.core.domain.usecase
 
+import com.depromeet.team5.core.domain.model.RetrospectionList
 import com.depromeet.team5.core.domain.repository.HedgeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RetrospectionListUseCase @Inject constructor(
     private val repository: HedgeRepository
 ){
 
-    operator fun invoke() = repository.retrospectionList()
+    operator fun invoke(): Flow<RetrospectionList> = repository.retrospectionList()
 }
--- core/domain/src/main/java/com/depromeet/team5/core/domain/usecase/UserStatsUseCase.kt
+++ core/domain/src/main/java/com/depromeet/team5/core/domain/usecase/UserStatsUseCase.kt
@@ -1,11 +1,13 @@
 package com.depromeet.team5.core.domain.usecase
 
+import com.depromeet.team5.core.domain.model.UserStats
 import com.depromeet.team5.core.domain.repository.HedgeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserStatsUseCase @Inject constructor(
     private val repository: HedgeRepository
 ) {
 
-    operator fun invoke() = repository.userStats()
+    operator fun invoke(): Flow<UserStats> = repository.userStats()
 }

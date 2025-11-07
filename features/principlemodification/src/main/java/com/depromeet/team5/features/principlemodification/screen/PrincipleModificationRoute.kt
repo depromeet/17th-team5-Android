8f8483e2071e98aa2f49637904f210755943a724
seoganghwi <skh0818@hanyang.ac.kr>
2025-11-07 11:46:40 +0900

    일부 UI 수정

--- features/principlemodification/src/main/java/com/depromeet/team5/features/principlemodification/screen/PrincipleModificationRoute.kt
+++ features/principlemodification/src/main/java/com/depromeet/team5/features/principlemodification/screen/PrincipleModificationRoute.kt
@@ -14,17 +14,18 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,10 +84,6 @@ private fun PrincipleModificationScreen(
             )
         }
 
-        is HedgeUiState.Loading -> {
-
-        }
-
         is HedgeUiState.Error -> {
             onBackClicked()
 
@@ -94,6 +91,8 @@ private fun PrincipleModificationScreen(
                 onShowErrorToast(it)
             }
         }
+
+        else -> {}
     }
 }
 
@@ -104,8 +103,19 @@ private fun PrincipleModificationContent(
     onClickedConfirmButton: () -> Unit,
     onBackClicked: () -> Unit
 ) {
-    var text by rememberSaveable {
-        mutableStateOf(principleModification.description)
+    var principle by remember {
+        mutableStateOf(
+            TextFieldState(
+                initialText = principleModification.principle
+            )
+        )
+    }
+    var content by remember {
+        mutableStateOf(
+            TextFieldState(
+                initialText = principleModification.description
+            )
+        )
     }
 
     Column(
@@ -125,13 +135,15 @@ private fun PrincipleModificationContent(
                 .height(12.dp)
         )
 
-        Text(
+        BasicTextField(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 10.dp),
-            text = principleModification.principle,
-            style = HedgeTypography.Headline1.SemiBold,
-            color = HedgeColor.Text.Title
+            state = principle,
+            textStyle = HedgeTypography.Headline1.SemiBold.copy(
+                color = HedgeColor.Text.Title
+            ),
+            cursorBrush = SolidColor(HedgeColor.Brand.Darken)
         )
 
         HorizontalDivider(
@@ -143,14 +155,13 @@ private fun PrincipleModificationContent(
 
         BasicTextField(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(horizontal = 20.dp, vertical = 24.dp),
-            value = text,
-            onValueChange = {
-                text = it
-            },
+            state = content,
             textStyle = HedgeTypography.Body3.Regular.copy(
                 color = HedgeColor.Text.Title
-            )
+            ),
+            cursorBrush = SolidColor(HedgeColor.Brand.Darken)
         )
     }
 }

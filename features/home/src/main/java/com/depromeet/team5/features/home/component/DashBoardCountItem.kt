d05268a22b7882f335037d74afbc66490359e4cd
MinseoSONG <mins041600@naver.com>
2025-11-08 02:19:44 +0900

    불필요한 import 구문 삭제

--- features/home/src/main/java/com/depromeet/team5/features/home/component/DashBoardCountItem.kt
+++ features/home/src/main/java/com/depromeet/team5/features/home/component/DashBoardCountItem.kt
@@ -3,16 +3,12 @@ package com.depromeet.team5.features.home.component
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview

538f8bf6056a91e9d11ae961293b4f30d7096ede
MinseoSONG <mins041600@naver.com>
2025-11-08 17:31:42 +0900

    로그인 모듈 생성
    # Conflicts:
    #	settings.gradle.kts

---
+++ features/login/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.depromeet.team5.feature)
+    alias(libs.plugins.depromeet.team5.hilt)
+}
+
+android {
+    namespace = "com.konkuk.login"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.domain)
+
+    implementation(projects.core.navigation)
+    implementation(projects.core.ui)
+    implementation(projects.core.domain)
+
+    implementation(libs.coil.compose)
+}

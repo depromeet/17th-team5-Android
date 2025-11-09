5280377e4cad87e97d7ab2e5db40c2bfc01df76b
MinseoSONG <mins041600@naver.com>
2025-11-09 18:58:33 +0900

    스플래시 화면 구현

---
+++ features/login/src/main/java/com/depromeet/team5/features/login/LoginScreen.kt
@@ -0,0 +1,25 @@
+package com.depromeet.team5.features.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginRoute(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+private fun LoginScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen()
+}
--- features/login/src/main/java/com/depromeet/team5/features/login/SplashScreen.kt
+++ features/login/src/main/java/com/depromeet/team5/features/login/SplashScreen.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun SplashScreen(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier

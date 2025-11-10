fa3bdab9acc76d31268a090295205cd3cbf7141c
dudwls901 <dudwls901@naver.com>
2025-11-11 00:33:03 +0900

    HedgeBadge thumbnailRes 추가

--- core/ui/src/main/java/com/depromeet/team5/core/ui/model/HedgeBadge.kt
+++ core/ui/src/main/java/com/depromeet/team5/core/ui/model/HedgeBadge.kt
@@ -8,26 +8,31 @@ import com.depromeet.team5.core.ui.R
 @Immutable
 enum class HedgeBadge(
     @DrawableRes val iconRes: Int,
+    @DrawableRes val thumbnailRes: Int,
     @StringRes val titleRes: Int,
     @StringRes val descriptionRes: Int
 ) {
     BRONZE(
         iconRes = R.drawable.img_badge_bronze,
+        thumbnailRes = R.drawable.img_badge_bronze_thumbnail,
         titleRes = R.string.badge_title_bronze,
         descriptionRes = R.string.badge_description_bronze
     ),
     SILVER(
         iconRes = R.drawable.img_badge_silver,
+        thumbnailRes = R.drawable.img_badge_silver_thumbnail,
         titleRes = R.string.badge_title_silver,
         descriptionRes = R.string.badge_description_silver
     ),
     GOLD(
         iconRes = R.drawable.img_badge_gold,
+        thumbnailRes = R.drawable.img_badge_gold_thumbnail,
         titleRes = R.string.badge_title_gold,
         descriptionRes = R.string.badge_description_gold
     ),
     PLATINUM(
         iconRes = R.drawable.img_badge_platinum,
+        thumbnailRes = R.drawable.img_badge_platinum_thumbnail,
         titleRes = R.string.badge_title_platinum,
         descriptionRes = R.string.badge_description_platinum
     );

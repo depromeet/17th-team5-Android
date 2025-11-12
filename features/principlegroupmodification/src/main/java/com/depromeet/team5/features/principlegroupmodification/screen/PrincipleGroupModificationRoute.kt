29b73f3cf88186404e0d9f4571c077a573475bac
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-11-12 14:42:01 +0900

    새 원칙 그룹 UI 구현

---
+++ features/principlegroupmodification/src/main/java/com/depromeet/team5/features/principlegroupmodification/screen/PrincipleGroupModificationRoute.kt
@@ -0,0 +1,366 @@
+package com.depromeet.team5.features.principlegroupmodification.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.depromeet.team5.core.designsystem.component.HedgeButton
+import com.depromeet.team5.core.designsystem.component.HedgeTopBar
+import com.depromeet.team5.core.designsystem.foundation.HedgeColor
+import com.depromeet.team5.core.designsystem.foundation.HedgeIcon
+import com.depromeet.team5.core.designsystem.foundation.HedgeTypography
+import com.depromeet.team5.features.principlegroupmodification.R
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun PrincipleGroupModificationRoute(
+    modifier: Modifier = Modifier,
+    viewModel: PrincipleGroupModificationViewModel = hiltViewModel(),
+    onShowToast: (String) -> Unit,
+    onShowErrorToast: (Throwable) -> Unit,
+    onBackPressed: () -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val groupName by viewModel.groupNameState.stateFlow.collectAsStateWithLifecycle()
+    val thumbnail by viewModel.thumbnailState.stateFlow.collectAsStateWithLifecycle()
+
+    val isConfirmButtonEnabled by remember {
+        derivedStateOf {
+            viewModel.distinct(groupName, thumbnail)
+        }
+    }
+
+    LaunchedEffect(lifecycleOwner) {
+        lifecycleOwner.lifecycleScope.launch {
+            viewModel.eventFlow
+                .flowWithLifecycle(lifecycleOwner.lifecycle)
+                .collect { throwable ->
+                    onShowErrorToast(throwable)
+                    onBackPressed()
+                }
+        }
+    }
+
+    PrincipleGroupModificationScreen(
+        groupName = groupName,
+        selectedEmoji = thumbnail,
+        isConfirmButtonEnabled = isConfirmButtonEnabled,
+        modifier = modifier,
+        onClickedEmoji = { selectedEmoji ->
+            viewModel.updateThumbnail(selectedEmoji)
+        },
+        onClickedConfirmButton = {},
+        onUpdateGroupName = { newGroupName ->
+            viewModel.updateGroupName(newGroupName)
+        },
+        onShowToast = onShowToast,
+        onBackPressed = onBackPressed
+    )
+}
+
+@Composable
+fun PrincipleGroupModificationScreen(
+    groupName: String,
+    selectedEmoji: String,
+    isConfirmButtonEnabled: Boolean,
+    modifier: Modifier = Modifier,
+    onClickedEmoji: (String) -> Unit,
+    onClickedConfirmButton: () -> Unit,
+    onUpdateGroupName: (String) -> Unit,
+    onShowToast: (String) -> Unit,
+    onBackPressed: () -> Unit
+) {
+    val context = LocalContext.current
+    val emojiList = stringArrayResource(R.array.emoji_array)
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(HedgeColor.WHITE)
+            .imePadding()
+    ) {
+        HedgeTopBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.principle_group_modification_title),
+                    style = HedgeTypography.Body3.SemiBold,
+                    color = HedgeColor.Text.Primary
+                )
+            },
+            back = {
+                Image(
+                    imageVector = HedgeIcon.ArrowLeftThick,
+                    contentDescription = null
+                )
+            },
+            action = {
+                HedgeButton.Text(
+                    text = stringResource(id = R.string.complete),
+                    enabled = isConfirmButtonEnabled,
+                    forceClickable = isConfirmButtonEnabled,
+                    imageVector = null,
+                    onClick = onClickedConfirmButton
+                )
+            },
+            onClickBack = onBackPressed
+        )
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(8.dp)
+        )
+
+        DisableMarkerBasicTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 18.dp),
+            value = groupName,
+            onValueChange = { new ->
+                if (new.length > 20) {
+                    onShowToast(
+                        context.getString(
+                            R.string.principle_group_modification_limit_group_name_count
+                        )
+                    )
+                } else {
+                    onUpdateGroupName(new)
+                }
+            },
+            singleLine = true,
+            textStyle = HedgeTypography.Body2.SemiBold.copy(
+                color = HedgeColor.Text.Title
+            ),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        if (groupName.isEmpty()) {
+                            Text(
+                                text = stringResource(R.string.principle_group_modification_placeholder),
+                                style = HedgeTypography.Body2.SemiBold,
+                                color = HedgeColor.Text.Assistive
+                            )
+
+                        } else {
+                            innerTextField()
+                        }
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .padding(start = 18.dp),
+                        text = "${groupName.length} / 20",
+                        style = HedgeTypography.Label2.Medium,
+                        color = HedgeColor.Text.Assistive
+                    )
+                }
+            }
+        )
+
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .background(
+                    color = if (groupName.isNotEmpty()) {
+                        HedgeColor.Brand.Primary
+                    } else {
+                        HedgeColor.Neutral.BackgroundSecondary
+                    }
+                ),
+            thickness = 1.dp,
+            color = HedgeColor.Text.Disabled
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(start = 20.dp, top = 30.dp, bottom = 10.dp),
+            text = stringResource(R.string.principle_group_modification_icon_title),
+            style = HedgeTypography.Body2.Medium,
+            color = HedgeColor.Text.Title
+        )
+
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxWidth(),
+            columns = GridCells.Fixed(7),
+            horizontalArrangement = Arrangement.spacedBy(9.dp),
+            verticalArrangement = Arrangement.spacedBy(14.dp),
+            contentPadding = PaddingValues(start = 20.dp, top = 4.dp, end = 20.dp)
+        ) {
+            items(
+                items = emojiList,
+                key = { it }
+            ) { emoji ->
+                GridItem(
+                    emoji = emoji,
+                    isSelected = emoji == selectedEmoji,
+                    onClickedEmoji = onClickedEmoji
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun GridItem(
+    emoji: String,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    onClickedEmoji: (String) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .clickable(
+                enabled = true,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onClickedEmoji(emoji)
+            }
+            .border(
+                width = 2.dp,
+                color = if (isSelected) {
+                    HedgeColor.Brand.Primary
+                } else {
+                    HedgeColor.Transparent
+                },
+                shape = CircleShape
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(7.dp),
+            text = emoji,
+            fontSize = 26.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+@Composable
+fun DisableMarkerBasicTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = HedgeTypography.Body3.Regular
+        .copy(color = HedgeColor.Text.Title),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    interactionSource: MutableInteractionSource? = null,
+    cursorBrush: Brush = SolidColor(HedgeColor.Brand.Darken),
+    decorationBox: @Composable (innerTextField: @Composable () -> Unit) -> Unit =
+        @Composable { innerTextField -> innerTextField() },
+) {
+
+    val transparentSelectionColors = remember {
+        TextSelectionColors(
+            handleColor = Color.Transparent,
+            backgroundColor = Color.Transparent
+        )
+    }
+
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides transparentSelectionColors
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = modifier,
+            enabled = enabled,
+            readOnly = readOnly,
+            textStyle = textStyle,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            minLines = minLines,
+            visualTransformation = visualTransformation,
+            onTextLayout = onTextLayout,
+            interactionSource = interactionSource,
+            cursorBrush = cursorBrush,
+            decorationBox = decorationBox
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrincipleGroupModificationScreenPreview() {
+    PrincipleGroupModificationScreen(
+        groupName = "",
+        selectedEmoji = "",
+        isConfirmButtonEnabled = false,
+        onClickedConfirmButton = {},
+        onUpdateGroupName = {},
+        onClickedEmoji = {},
+        onShowToast = {},
+        onBackPressed = {}
+    )
+}
---
+++ features/principlegroupmodification/src/main/java/com/depromeet/team5/features/principlegroupmodification/screen/PrincipleGroupModificationViewModel.kt
@@ -0,0 +1,68 @@
+package com.depromeet.team5.features.principlegroupmodification.screen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.depromeet.team5.core.domain.model.MyPrincipleGroup
+import com.depromeet.team5.core.domain.usecase.GetPrincipleGroupUseCase
+import com.depromeet.team5.core.ui.extensions.baseCollect
+import com.depromeet.team5.core.ui.lazy.hedgeState
+import com.depromeet.team5.features.principlegroupmodification.navigation.PrincipleGroupModification
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class PrincipleGroupModificationViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val getPrincipleGroupUseCase: GetPrincipleGroupUseCase
+) : ViewModel() {
+
+    var originalGroupName = MyPrincipleGroup.EMPTY
+        private set
+
+    val groupNameState by hedgeState("")
+    val thumbnailState by hedgeState("")
+
+    private val _errorFlow = MutableSharedFlow<Throwable>()
+    val eventFlow = _errorFlow.asSharedFlow()
+
+
+    init {
+        viewModelScope.launch {
+            flow {
+                emit(savedStateHandle.toRoute<PrincipleGroupModification>())
+            }
+                .flatMapLatest {
+                    getPrincipleGroupUseCase(it.groupId)
+                }
+                .baseCollect(
+                    onSuccess = { myPrincipleGroup ->
+                        originalGroupName = myPrincipleGroup.copy()
+                        groupNameState.update { myPrincipleGroup.groupName }
+                        thumbnailState.update { myPrincipleGroup.thumbnail }
+                    },
+                    onError = {
+                        _errorFlow.emit(it)
+                    }
+                )
+        }
+    }
+
+    fun updateGroupName(newGroupName: String) {
+        groupNameState.update { newGroupName }
+    }
+
+    fun updateThumbnail(thumbnail: String) {
+        thumbnailState.update { thumbnail }
+    }
+
+    fun distinct(groupName: String, thumbnail: String) =
+        originalGroupName.groupName != groupName || originalGroupName.thumbnail != thumbnail
+}

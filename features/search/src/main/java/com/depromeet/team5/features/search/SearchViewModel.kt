0998c0233df96dd765f558223cba85203d3225b6
MinseoSONG <mins041600@naver.com>
2025-11-12 23:10:07 +0900

    데이터 반영

--- features/search/src/main/java/com/depromeet/team5/features/search/SearchViewModel.kt
+++ features/search/src/main/java/com/depromeet/team5/features/search/SearchViewModel.kt
@@ -16,11 +16,9 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -40,15 +38,12 @@ class SearchViewModel @Inject constructor(
         retrospectionListUseCase()
             .map { list ->
                 list.data
-                    .map { it.companyName }
-                    .distinct()
-                    .sorted()
-                    .map { company ->
+                    .map { stock ->
                         StockData(
-                            symbol = "",
-                            stockName = company,
-                            market = "",
-                            stockImageUrl = null
+                            symbol = stock.symbol,
+                            stockName = stock.companyName,
+                            market = stock.market,
+                            stockImageUrl = stock.image
                         )
                     }
             }

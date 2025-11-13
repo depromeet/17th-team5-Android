70085b9b6149b7b6f0867d8d3eef8de4cd8cfcfd
dudwls901 <dudwls901@naver.com>
2025-11-13 22:29:35 +0900

    스테이터스바 light mode 고정으로 컬러 대응

--- app/src/main/java/com/depromeet/team5/MainActivity.kt
+++ app/src/main/java/com/depromeet/team5/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.depromeet.team5
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
@@ -29,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.light(TRANSPARENT, TRANSPARENT))
         setContent {
             DepromeetTheme {
                 val context = LocalContext.current

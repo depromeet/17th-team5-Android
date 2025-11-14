ca15882a1886fbe6c327658794f293f4aaa64fe2
tjrkdgnl <skh0818@hanyang.ac.kr>
2025-11-14 16:36:12 +0900

    리뷰반영-coil-okhttp 라이브러리 제거

--- features/principlegroupmodification/build.gradle.kts
+++ features/principlegroupmodification/build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation(projects.core.ui)
 
     implementation(libs.coil.compose)
-    implementation(libs.coil.network.okhttp)
 
     implementation(libs.org.jetbrains.kotlinx.kotlinx.serialization.json)
 }

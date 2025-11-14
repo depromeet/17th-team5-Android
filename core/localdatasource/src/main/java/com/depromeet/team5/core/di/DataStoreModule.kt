15266c060b1b3ce48c3532fea02a453bb9a2be7d
seoganghwi <skh0818@hanyang.ac.kr>
2025-11-15 00:24:27 +0900

    DataStoreModule.kt 모듈 localdatasource 모듈로 이동

--- core/localdatasource/src/main/java/com/depromeet/team5/core/di/DataStoreModule.kt
+++ core/localdatasource/src/main/java/com/depromeet/team5/core/di/DataStoreModule.kt
@@ -1,28 +1,41 @@
-package com.depromeet.team5.di
+package com.depromeet.team5.core.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 import javax.inject.Singleton
-
-private const val BADGE_DOT_FILE = "badge_dot_prefs.preferences_pb"
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataStoreModule {
+internal object DataStoreModule {
+
+    private const val BADGE_DOT_FILE = "badge_dot_prefs"
+    private const val LOGIN_TOKEN_FILE = "login_token_file"
+
 
     @Provides
     @Singleton
-    fun providePreferencesDataStore(
+    @Named(NamedKey.BADGE_DOT_FILE)
+    fun providePreferencesDataStoreForBadge(
         @ApplicationContext context: Context
     ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
         produceFile = { context.preferencesDataStoreFile(BADGE_DOT_FILE) }
+    )
+
+    @Provides
+    @Singleton
+    @Named(NamedKey.LOGIN_TOKEN)
+    fun providePreferencesDataStoreForLogin(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
+        produceFile = { context.preferencesDataStoreFile(LOGIN_TOKEN_FILE) }
     )
 }

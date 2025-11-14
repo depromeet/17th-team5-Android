3cf57157e3aeb696d7244710e752c4e89e965bb5
MinseoSONG <mins041600@naver.com>
2025-11-14 20:52:55 +0900

    setting feature 모듈 추가
    # Conflicts:
    #	settings.gradle.kts

---
+++ features/setting/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.depromeet.team5.feature)
+    alias(libs.plugins.depromeet.team5.hilt)
+}
+
+android {
+    namespace = "com.depromeet.team5.features.setting"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.ui)
+    implementation(projects.core.domain)
+
+    implementation(libs.coil.compose)
+}

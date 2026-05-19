42ac545916934bb7f9f1fa0821bd92f0f5b83834
seoganghwi <skh0818@hanyang.ac.kr>
2026-05-19 21:27:15 +0900

    버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 
     defaultConfig {
         applicationId = "com.depromeet.team5"
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
